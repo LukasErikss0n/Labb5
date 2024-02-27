18686883abf2fbb007c7a1aa6eb9c61d0acc2dfe
lukaserikss0n <lukas.emil.eriksson@outlook.com>
2024-02-27 09:43:02 +0100

    First

---
+++ src/Situation.java
@@ -0,0 +1,9 @@
+public class Situation {
+    State state = State.Input1;
+    JLabel display;
+    BinOpButton binaryOperator;
+    int leftOperand;
+    Situation(JLabel display){
+        this.display = display;
+    }
+}
